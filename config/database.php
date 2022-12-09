e3482d984046e71c1052a11d30025a73bb0de558
Lusaca87 <christian.delta@gmx.net>
2022-12-09 17:04:30 +0100

    .gitignore is now working

--- config/database.php
+++
@@ -1,9 +0,0 @@
-<?php
-
-$dbhost = "localhost"; // Your Database server, mostly localhost
-$dbuser = ""; // Your database username
-$dbpass = ""; // Your database user password
-$dbbase = ""; // Your Database
-
-
-?>
